146dfc0c5001a035c80b32c8c3fb965f591d5234
neuralrunner <brunohcagnin@gmail.com>
2020-05-07 20:10:04 -0300

    Criação de novas Activities para o Projeto, assim como seus respectivos Layouts.

---
+++ app/src/main/java/codes/neuralkatana/tupicionario/CulinariaActivity.java
@@ -0,0 +1,14 @@
+package codes.neuralkatana.tupicionario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CulinariaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_culinaria);
+    }
+}
