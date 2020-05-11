a4dd657e315f955832049a292cd161e84b4d8ef0
neuralrunner <brunohcagnin@gmail.com>
2020-05-11 11:55:33 -0300

    Adicionando activity_povos_nativos.xml, assim como ações para todos os TextViews do Projeto na MainActivity. Mudança de Nome da PlantasActivity por causa de um Typo.

--- app/src/main/java/codes/neuralkatana/tupicionario/MainActivity.java
+++ app/src/main/java/codes/neuralkatana/tupicionario/MainActivity.java
@@ -2,13 +2,52 @@ package codes.neuralkatana.tupicionario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvBichos;
+    private TextView tvCulinaria;
+    private TextView tvPlantas;
+    private TextView tvPovosNativos;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvBichos = findViewById(R.id.tvBichos);
+        tvCulinaria = findViewById(R.id.tvCulinaria);
+        tvPlantas = findViewById(R.id.tvPlantas);
+        tvPovosNativos = findViewById(R.id.tvPovosNativos);
+    }
+    /*Actions for the TextViews.
+    Those are setup at the XML for Cleaner code.*/
+
+    //tvBichos click action
+    public void onClickBichos(View view){
+        intent = new Intent(MainActivity.this, BichosActivity.class);
+        startActivity(intent);
+    }
+
+    //tvCulinaria click action
+    public void onClickCulinaria(View view){
+        intent = new Intent(MainActivity.this, CulinariaActivity.class);
+        startActivity(intent);
+    }
+
+    //tvPlantas click action
+    public void onClickPlantas(View view){
+        intent = new Intent(MainActivity.this, PlantasActivity.class);
+        startActivity(intent);
+    }
+
+    //tvPovosNativos click action
+    public void onClickPovosNativos(View view){
+        intent = new Intent(MainActivity.this, PovosNativosActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/codes/neuralkatana/tupicionario/PlantasActivity.java
+++ app/src/main/java/codes/neuralkatana/tupicionario/PlantasActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Plantas_Activity extends AppCompatActivity {
+public class PlantasActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
